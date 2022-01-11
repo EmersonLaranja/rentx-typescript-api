f03094a357a257d8b6243565d57c67ab5086d0c6
EmersonLaranja <emersonlaranja@gmail.com>
2022-01-11 11:45:03 -0300

    feat: create import use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoute = Router();
 
+const upload = multer({
+  dest: "./tmp",
+});
+
 categoriesRoute.post("/", (req, res) =>
   createCategoryController.handle(req, res)
 );
+
+categoriesRoute.post("/import", upload.single("file"), (req, res) => {
+  importCategoryController.handle(req, res);
+});
 
 categoriesRoute.get("/", (req, res) =>
   listCategoriesController.handle(req, res)
