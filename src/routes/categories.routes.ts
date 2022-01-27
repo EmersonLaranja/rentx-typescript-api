2169d1585690b02cf75989ade34c9f1c2d198033
EmersonLaranja <emersonlaranja@gmail.com>
2022-01-27 15:27:03 -0300

    refact: import & list categories use injection dependency with tsyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,22 +2,24 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
+import { ListCategoriesController } from "../modules/cars/useCases/listCategories/ListCategoriesController";
 
 const categoriesRoute = Router();
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
+const listCategoriesController = new ListCategoriesController();
 const upload = multer({
   dest: "./tmp",
 });
 
 categoriesRoute.post("/", createCategoryController.handle);
 
-categoriesRoute.post("/import", upload.single("file"), (req, res) => {
-  importCategoryController.handle(req, res);
-});
-
-categoriesRoute.get("/", (req, res) =>
-  listCategoriesController.handle(req, res)
+categoriesRoute.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
 );
+
+categoriesRoute.get("/", listCategoriesController.handle);
 export { categoriesRoute };
