2ca8741393032c6b2fcdde7c25a9fd5e4b373b03
EmersonLaranja <emersonlaranja@gmail.com>
2022-03-10 05:53:27 -0300

    refact: imports with @ & folders meaningful

---
+++ src/shared/infra/http/routes/users.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import multer from "multer";
+
+import uploadConfig from "@config/upload";
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const usersRoutes = Router();
+const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+usersRoutes.post("/", createUserController.handle);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
+
+export { usersRoutes };
