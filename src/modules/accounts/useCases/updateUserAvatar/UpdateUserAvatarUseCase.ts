b6df9007062716c5f051e1bd3d97fb7aaf0c44c6
EmersonLaranja <emersonlaranja@gmail.com>
2022-03-10 05:49:27 -0300

    feat: update an user avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repository/IUsersRepository";
+import { deleteFile } from "@utils/file.";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository") private userRepository: IUsersRepository
+  ) {}
+  async execute({ avatar_file, user_id }: IRequest): Promise<void> {
+    const user = await this.userRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+    user.avatar = avatar_file;
+    await this.userRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
